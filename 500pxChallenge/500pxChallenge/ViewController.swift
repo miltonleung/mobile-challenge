8107d50877e8ce33d397f0a3f3c576ba6e8c365e
Milton <milleung964@gmail.com>
2017-01-22 15:33:07 -0800

    Initital commit

---
+++ 500pxChallenge/500pxChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  500pxChallenge
+//
+//  Created by Milton Leung on 2017-01-22.
+//  Copyright © 2017 Milton Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
